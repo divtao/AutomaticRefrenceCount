b7f73c003785abd4a8037d4e9a985c9f761063ac
divtao <guohaitao90@126.com>
2016-03-17 08:49:12 +0800

    Initial Commit

---
+++ AutomaticRefrenceCount/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutomaticRefrenceCount
+//
+//  Created by DivTao on 16/3/17.
+//  Copyright © 2016年 HT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutomaticRefrenceCount/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutomaticRefrenceCount
+//
+//  Created by DivTao on 16/3/17.
+//  Copyright © 2016年 HT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
